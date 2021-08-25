6000e296d6554bd8102d0767302f230ff0511687
Pritam Behera <69230350+thelearn-tech@users.noreply.github.com>
2021-08-25 08:12:44 +0530

    Create ip.js

---
+++ assets/js/ip.js
@@ -0,0 +1,11 @@
+<script>
+		  // js for client ip address
+         $.getJSON("https://api.ipify.org?format=json", 
+         
+                                           function(data) {
+         
+             $("#usrip").html(data.ip); 
+         
+         }) 
+         
+      </script>  
